c0becca44805b6b8adf9a30007219dfa8ffe450e
adam.bohannon <adam.bohannon@housecallpro.com>
2025-07-10 13:00:05 -0700

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 // Create a client
 const queryClient = new QueryClient({
@@ -21,5 +22,6 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
+    <Analytics />
   </StrictMode>,
 )
